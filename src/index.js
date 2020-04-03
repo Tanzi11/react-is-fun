0786ab9a2f2b9a095cbc9f2a2d247e9054aaf4ec
Tanzi11 <tanzima.c@fellows.thedoschool.org>
2020-04-02 20:39:46 -0400

    create Message component

--- src/index.js
+++ src/index.js
@@ -1,21 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-var style = {
-  backgroundColor: 'orange',
-  color: 'white',
-  fontFamily: 'arial'
+class Message extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello Everyone</h1>
+      </div>
+    )
+  }
 }
-
-// const title = React.createElement(
-//   'h1',
-//   {id: 'title', className:'header', style: style},
-//   'Hello World'
-// )
-
 ReactDOM.render(
-  <div style={style}>
-    <h1 id="heading-element">Hello World!</h1>
-    <p>We're glad you're here!</p>
-  </div>,
+<Message />,
    document.getElementById('root'))
